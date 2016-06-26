ceea1c8ff109ccb6fa862ea07b8b095bbb2c053d
frane <frane.roje@gmail.com>
2016-06-26 17:29:14 +0100

    Modifying code deploy script for aws linux ami

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get upgrade -y
-
-# install dependencies
-apt-get install mercurial git gcc libc6-dev -y
-
 # install golang
 cd /home/ec2-user
-hg clone -u release https://code.google.com/p/go
-cd go/src
-./all.bash
+yum install go
