75d9acf4623207f3fc9a709568f12ffe0381c2b2
frane <frane.roje@gmail.com>
2016-06-27 22:17:22 +0100

    moved commands to run under correct user

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# make golang workspace
+echo "export GOPATH=/home/ubuntu/go" >> /home/ubuntu/.bashrc
+echo "export PATH=$PATH:/home/ubuntu/go/bin" >> /home/ubuntu/.bashrc
+echo "export PATH=$PATH:/usr/local/go/bin" >> /home/ubuntu/.bashrc
+
+# activate changes
+source /home/ubuntu/.bashrc
+
 # install dependencies
 whoami >> /home/ubuntu/log.txt
 pwd >> /home/ubuntu/log.txt
 echo $PATH >> /home/ubuntu/log.txt
-/usr/local/bin/go get github.com/gin-gonic/gin
+
+go get github.com/gin-gonic/gin
 
 # go to golang app
 cd /home/ubuntu/go/src/github.com/hoohawa/golang_web/
 
 # make binary
-/usr/local/bin/go install
+go install
--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -9,11 +9,3 @@ apt-get install git -y
 # install golang
 wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
-
-# make golang workspace
-echo "export GOPATH=/home/ubuntu/go" >> /home/ubuntu/.bashrc
-echo "export PATH=$PATH:/home/ubuntu/go/bin" >> /home/ubuntu/.bashrc
-echo "export PATH=$PATH:/usr/local/go/bin" >> /home/ubuntu/.bashrc
-
-# activate changes
-source /home/ubuntu/.bashrc
