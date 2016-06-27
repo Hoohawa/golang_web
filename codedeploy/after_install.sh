897dbaf1afde05f092939f58d3759b9d9036da4d
frane <frane.roje@gmail.com>
2016-06-27 22:01:29 +0100

    Removing uneccessary go packages

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -2,9 +2,6 @@
 
 # install dependencies
 go get github.com/gin-gonic/gin
-go get github.com/golang/protobuf/proto
-go get github.com/manucorporat/sse
-go get golang.org/x/net/context
 
 # go to golang app
 cd /home/ubuntu/go/src/github.com/hoohawa/golang_web/
