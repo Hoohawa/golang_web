f0e604a87422e93b8479984bf96a5735f870d0b0
frane <frane.roje@gmail.com>
2016-06-27 22:08:22 +0100

    Adding more debugging info

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # install dependencies
+whoami >> /home/ubuntu/log.txt
 pwd >> /home/ubuntu/log.txt
 echo $PATH >> /home/ubuntu/log.txt
-go get github.com/gin-gonic/gin
+/usr/local/bin/go get github.com/gin-gonic/gin
 
 # go to golang app
 cd /home/ubuntu/go/src/github.com/hoohawa/golang_web/
 
 # make binary
-go install
+/usr/local/bin/go install
